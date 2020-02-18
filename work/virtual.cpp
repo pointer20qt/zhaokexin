e3f06660652955b2bc48cf298cda9758231aa9e1
zhaokexin00 <791948188@qq.com>
2020-02-18 13:30:28 +0800

    智能指针

--- work/virtual.cpp
+++ work/virtual.cpp
@@ -1,177 +1,48 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
+#include<initializer_list>
 #include<string>
+#include<vector>
+using namespace std;
 
-
-//函数隐藏
-/*
-class school{
-	std::string schoolName;
-	std::string schoolType;
-	std::string address;
-public:
-	
-	void schoolopens()
-	{
-		cout << this->schoolName << endl;
-	}
-	void schoolopens(int i)
-	{
-		for (int tem = 0; tem < i; tem++)
-		{
-			cout << this->schoolName << endl;
-		}		
-	}
-};
-class university :public school
+//栈，先入后出，push\pop\top\size\empty
+template <class T> class L_stack
 {
+	std::vector<T> arr;
 public:
-	
-	void schoolopens()
+	void push(T&& obj)
 	{
-		cout << "隐藏父类" << endl;
+		arr.push_back(obj);
+	}
+	void pop()
+	{
+		arr.pop_back();
+	}
+	T& top()
+	{
+		return arr.back();
+	}
+	size_t size()//size_t===typedef unsigned int
+	{
+		return arr.size();
+	}
+	bool empty()
+	{
+		return arr.empty();
 	}
 };
 int main()
 {
-	university u;
-	u.schoolopens();	
-}
-*/
+	L_stack<int> l;
+	l.push(5);
+	l.push(6);
+	l.push(7);
+	cout << l.top() << endl;
+	l.pop();
+	cout << l.top() << endl;
+	cout << l.size() << endl;
 
-//虚函数、函数覆盖
-//父类指针、子类对象
-/*class school{
-	std::string schoolName;
-	std::string schoolType;
-	std::string address;
-public:
-	~school(){
-		cout << schoolName << "放假了" << endl;
-	}
-	school(std::string schoolName, std::string schoolType, std::string address){
-		cout << "普通构造函数" << endl;
-		this->schoolName = schoolName;
-		this->address = address;
-		this->schoolType = schoolType;
-	}
-	std::string getschoolname()
-	{
-		return schoolName;
-	}
-	virtual void schoolopens()
-	{
-		cout << this->schoolName << "开学" << endl;
-	}
-};
-
-class university :public school
-{
-public:
-	university() :school("东北石油大学", "大学", "大庆")
-	{
-
-	}
-	virtual void schoolopens()
-	{
-		cout << getschoolname() << "开学典礼" << endl;
-	}
-};
-//父类指针、子类对象
-//看父类是否是虚函数，如果是就调用重写后的
-//void printschool(school* school)
-//{
-//	school->schoolopens();
-//}
-void printschool(school& school)
-{
-	school.schoolopens();
+	cout << "运行结束" << endl;
 }
 
-int main(){
-	university u;
-	printschool(u);
-}
-*/
 
-class principal{
-	std::string name;
-	//...
-public:
-	principal(std::string sname){
-		this->name = sname;
-	}
-	~principal(){
-		cout << name << "下班了" << endl;
-	}
-};
-class school{
-	std::string schoolName;
-	std::string schoolType;
-	std::string address;
-public:
-	std::string getschoolname()
-	{
-		return schoolName;
-	}
-	principal* master;
-	virtual ~school(){
-		cout << schoolName << "放假了" << endl;
-		delete master;
-	}
-	school(std::string schoolName, std::string schoolType, std::string address){
-		cout << "普通构造函数" << endl;
-		this->schoolName = schoolName;
-		this->address = address;
-		this->schoolType = schoolType;
-		master = new principal("杨老师");
-	}
-	school(const school& p) :schoolName(p.schoolName), schoolType(p.schoolType), address(p.address){//初始化参数列表
-		schoolName = p.schoolName;
-		cout << this->schoolName << "拷贝建立了" << endl;
-		master = new principal(*(p.master));//new一块新的空间，并用新的对象拷贝构造
-	}
-	//类有默认的移动构造
-	school(school&& other) :schoolName(other.schoolName), schoolType(other.schoolType), address(other.address)
-	{
-		cout << "移动构造" << endl;
-		master = other.master;
-		other.master = NULL;
-	}
-	virtual void schoolopens()
-	{
-		cout << this->schoolName << "开学" << endl;
-	}
-};
-
-class university :public school
-{
-public:
-	principal* p;
-	university() :school("东北石油大学", "大学", "大庆")
-	{
-		cout << "大学的构造函数" << endl;
-		p = new principal("c++");
-	}
-	void schoolopens()
-	{
-		cout << getschoolname() << "开学典礼" << endl;
-	}
-	~university(){
-		cout << "大学的xigou函数" << endl;
-		delete p;
-	}
-};
-void printschool(school* school)
-{
-	school->schoolopens();
-	delete school;
-}
-
-int main(){
-	school *s=new school("春天幼儿园", "幼儿园", "春天");
-	university *u=new university();
-	printschool(u);
-
-}
 
